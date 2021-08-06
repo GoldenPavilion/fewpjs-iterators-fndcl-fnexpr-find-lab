863dc9ff39feefc117c3a3924a8ed9f73c894e86
GoldenPavilion <rcorystine@gmail.com>
2021-08-06 18:02:50 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-
+function superbowlWin(record) {
+    let win = record.find(function(obj){ 
+        return obj.result === "W"
+    })
+    if (win){
+        return win.year
+    } else {
+        return win
+    }
+}
